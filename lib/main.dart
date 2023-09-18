dd726efe2f6737a84ce84f82733b2538f36130cb
Reetish03 <145360110+Reetish03@users.noreply.github.com>
2023-09-18 21:00:15 +0530

    yeah this is my first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.red, // Set the background color to red
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            fontWeight: FontWeight.bold, // Make the text bold
+            fontStyle: FontStyle.italic, // Make the text slant (italic)
+          ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Hello World App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headline6, // Use the custom text style
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
